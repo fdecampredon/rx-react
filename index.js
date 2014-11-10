45cecab86a7f3b4eab8842f1d09cf6bec45d83bd
François de Campredon <francois.de.campredon@gmail.com>
2014-11-10 12:08:39 +0100

    remove action and property
    It will be moved to a separate repository

--- index.js
+++ index.js
@@ -1,9 +1,4 @@
-var Property        = require('./lib/property');
-var createClass     = require('./lib/createClass');
-var Action          = require('./lib/action');
 
 module.exports = {
-    Property: Property,
-    createClass: createClass,
-    Action: Action
+    createClass: require('./lib/createClass')
 };
