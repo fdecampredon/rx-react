c825ec19915e3d5bf0944a595a92eac3094d8256
François de Campredon <francois.de.campredon@gmail.com>
2015-01-28 13:02:59 +0100

    add base component for es6class

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 module.exports = {
+  Component: require('./lib/component'),
   LifecycleMixin: require('./lib/lifecycleMixin'),
   StateStreamMixin: require('./lib/stateStreamMixin').StateStreamMixin,
   EventHandler: require('./lib/eventHandler'),
