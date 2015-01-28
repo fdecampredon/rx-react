333e9e0915915800bcc3bea1d44c09b71fd6e61c
François de Campredon <francois.de.campredon@gmail.com>
2015-01-28 13:17:37 +0100

    expose the props mixin

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ module.exports = {
   Component: require('./lib/component'),
   LifecycleMixin: require('./lib/lifecycleMixin'),
   StateStreamMixin: require('./lib/stateStreamMixin').StateStreamMixin,
+  PropsMixin : require('./lib/propsMixin'),
   EventHandler: require('./lib/eventHandler'),
   FuncSubject : require('./lib/funcSubject'),
   renderToStringAsync: require('./lib/renderToStringAsync')
