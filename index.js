04a1a429bf50a0ec2dec635c92f6e3b140e53108
François de Campredon <francois.de.campredon@gmail.com>
2015-08-10 10:27:05 +0200

    introduce the DataStreamMixin

--- index.js
+++ index.js
@@ -18,6 +18,7 @@ module.exports = {
   Component: require('./lib/component'),
   LifecycleMixin: require('./lib/lifecycleMixin'),
   StateStreamMixin: require('./lib/stateStreamMixin').StateStreamMixin,
+  DataStreamMixin: require('./lib/dataStreamMixin').DataStreamMixin,
   PropsMixin: require('./lib/propsMixin'),
   FuncSubject: require('./lib/funcSubject'),
   createClass: require('./lib/createClass')
