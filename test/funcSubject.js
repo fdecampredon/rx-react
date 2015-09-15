d500c7e7d2651f4790284c9f5b261e56fa39380b
François de Campredon <francois.de.campredon@gmail.com>
2015-09-15 17:32:34 +0200

    complete FuncSubject tests

--- test/funcSubject.js
+++ test/funcSubject.js
@@ -17,9 +17,10 @@
 var test = require('tape');
 var FuncSubject = require('../').FuncSubject;
 var Rx = require('rx');
+var sinon = require('sinon');
 
 test('FuncSubject', function (t) {
-  t.plan(2);
+  
   function hasAllProperties(objA, objB) {
     var key;
     for (key in objB) {
@@ -29,16 +30,58 @@ test('FuncSubject', function (t) {
     }
     return true;
   }
-  t.ok(hasAllProperties(FuncSubject.create(), Rx.Subject.prototype), 'it should create an Rx Subject');
   
-  var value = {};
-  var eventHandler = FuncSubject.create();
-  
-  eventHandler.subscribe(function (val) {
-    t.equals(val, value, 'calling the handler as a function should invoque the \'onNext\' method ');
+  t.test('basic', function (t) {
+    t.plan(2);
+    
+    t.ok(hasAllProperties(FuncSubject.create(), Rx.Subject.prototype), 'it should create an Rx Subject');
+
+    var value = {};
+    var eventHandler = FuncSubject.create();
+
+    eventHandler.subscribe(function (val) {
+      t.equals(val, value, 'calling the handler as a function should invoque the \'onNext\' method ');
+    });
+
+    eventHandler(value);
   });
   
-  eventHandler(value);
+   
+  t.test('behavior', function (t) {
+    t.plan(3);
+    t.ok(hasAllProperties(FuncSubject.behavior(), Rx.BehaviorSubject.prototype), 'it should create an Rx BehaviorSubject');
+
+    var value = {};
+    var eventHandler = FuncSubject.behavior(value);
+    t.equals(eventHandler.getValue(), value, 'it should hold the initial value');
+    
+    var value2 = {};
+    eventHandler(value2);
+    eventHandler.subscribe(function (val) {
+      t.equals(val, value2, 'it should hold the last value called with');
+    });
+  });
   
+  t.test('transform function', function (t) {
+    t.plan(2);
+    
+    var value = {};
+    var value2 = {};
+    
+    var spy = sinon.spy(function () {
+      return value2;
+    });
+    
+    var eventHandler = FuncSubject.create(spy);
+    
+    eventHandler(value);
+    t.ok(spy.called, 'it should call the map function event if the observable holds no subscription');
+    
+    eventHandler.subscribe(function (val) {
+      t.equals(val, value2, 'calling the handler as a function should invoque the \'onNext\' method with mapped value');
+    });
+
+    eventHandler(value);
+  });
 });
 
